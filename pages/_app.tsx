a8603f2e8a71472b1cf82f2af69566b2a480d278
Edgar Cresson <e.cresson@groupe-projex.fr>
2022-12-19 14:19:49 +0100

    Finish Maïa setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../../../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { TopBar } from '@projex/ui';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Maïa</title>
+      </Head>
+      <header>
+        <TopBar />
+      </header>
+      <main>
+        <nav>Navigation</nav>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
