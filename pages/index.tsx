e78747dbe4a80f5007906584aefc4fad73c0be7f
Edgar Cresson <e.cresson@groupe-projex.fr>
2023-01-10 15:41:18 +0100

    Finished AffairCard component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,23 +3,17 @@ import styles from '../styles/Home.module.css';
 import { PageHeaderBanner } from '@projex/ui';
 import ProjectCard from '../src/components/ProjectCard/ProjectCard';
 import { CompanyEntity } from '../models/ProjectModel';
+import AffairCard from '../src/components/AffairCard/AffairCard';
 
 export default function Home() {
   return (
     <>
       <PageHeaderBanner title="Bonjour, Edgar Cresson" />
-      <div style={{ height: '100%', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <div style={{ width: '416px', height: '172px' }}>
-          <ProjectCard
-            project={{
-              name: 'Nom du projet',
-              client_company_name: 'Nom du client',
-              company_entity: CompanyEntity.PROJEX,
-            }}
-            projectManagerName="Edgar"
-          />
+      {/* <div style={{ height: '100%', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <div style={{ width: '416px', minHeight: '172px' }}>
+          <AffairCard affair={{ name: "Nom de l'affaire", internal_company: 'diagobat' }} />
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
--- src/components/AffairCard/AffairCard.tsx
+++ src/components/AffairCard/AffairCard.tsx
@@ -1,13 +1,67 @@
 import React from 'react';
 import styles from './AffairCard.module.scss';
 import { AffairModel } from '../../../models/AffairModel';
+import { ProgressBar, ShadowCard } from '@projex/ui';
+import kebabMenu from '../../../public/ellipsis-vertical.svg';
+import logoGroupeProjex from '../../../public/logo-groupe-projex.svg';
+import logoAmexia from '../../../public/logo-amexia.svg';
+import logoDiagobat from '../../../public/logo-diagobat.svg';
+import logoImperium from '../../../public/logo-imperium.svg';
+import logoProbim from '../../../public/logo-probim.svg';
+import logoProjex from '../../../public/logo-projex.svg';
+import { Percentage } from '@projex/ui/dist/components/atoms/ProgressBar/ProgressBar';
 
 type Props = {
   affair: AffairModel;
 };
 
 const AffairCard = ({ affair }: Props) => {
-  return <div className={styles.affairCard}>AffairCard</div>;
+  // TODO
+  const STEPS = [{ status: 'DONE' }, { status: 'DONE' }, { status: 'IN_PROGRESS' }];
+  const STEPS_COUNT = STEPS.length;
+  const FINISHED_STEPS_COUNT = STEPS.filter((step) => step.status === 'DONE').length;
+  const FINISHED_STEPS_COUNT_PERCENTAGE: Percentage = Math.floor((FINISHED_STEPS_COUNT / STEPS_COUNT) * 100);
+
+  const getImageSrc = () => {
+    switch (affair.internal_company) {
+      case 'amexia':
+        return logoAmexia.src;
+      case 'diagobat':
+        return logoDiagobat.src;
+      case 'imperium':
+        return logoImperium.src;
+      case 'probim':
+        return logoProbim.src;
+      case 'projex':
+        return logoProjex.src;
+      default:
+        return logoGroupeProjex.src;
+    }
+  };
+
+  return (
+    <ShadowCard>
+      <div className={styles.affairCard}>
+        <div className={styles.body}>
+          <h4 className={styles.title}>{affair.name}</h4>
+          <span>Chef de projet</span>
+          <div className={styles.imageContainer}>
+            <img src={getImageSrc()} alt={`Logo de l'entité ${affair.internal_company}`} />
+          </div>
+        </div>
+        <div className={styles.footer}>
+          {/* TODO */}
+          <span>
+            Étapes terminées : {FINISHED_STEPS_COUNT}/{STEPS_COUNT}
+          </span>
+          <ProgressBar percentage={FINISHED_STEPS_COUNT_PERCENTAGE} tiny />
+        </div>
+        <button className={styles.kebabButton}>
+          <img src={kebabMenu.src} alt={`Kebab menu for ${affair.name}`} />
+        </button>
+      </div>
+    </ShadowCard>
+  );
 };
 
 export default AffairCard;
