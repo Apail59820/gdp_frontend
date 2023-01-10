45bf39261be4c33eaec6269c8df7c693c0e9f60b
Edgar Cresson <e.cresson@groupe-projex.fr>
2023-01-10 14:37:30 +0100

    Finish ProjectCard

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,16 +7,19 @@ export default function Home() {
   return (
     <>
       <PageHeaderBanner title="Bonjour, Edgar Cresson" />
-      <div style={{ height: '100%', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      {/* <div style={{ height: '100%', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <div style={{ width: '416px', height: '172px' }}>
           <ProjectCard
             project={{
               name: 'Nom du projet',
               client_company_name: 'Nom du client',
             }}
+            projectManagerName="Edgar"
+            affairsCount={3}
+            company="projex"
           />
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -3,13 +3,38 @@ import styles from './ProjectCard.module.scss';
 import { ShadowCard } from '@projex/ui';
 import type { ProjectModel } from '../../../models/ProjectModel';
 import img from '../../../public/project-image.png';
+import logoGroupeProjex from '../../../public/logo-groupe-projex.svg';
+import logoAmexia from '../../../public/logo-amexia.svg';
+import logoDiagobat from '../../../public/logo-diagobat.svg';
+import logoImperium from '../../../public/logo-imperium.svg';
+import logoProbim from '../../../public/logo-probim.svg';
 import logoProjex from '../../../public/logo-projex.svg';
 
 type Props = {
   project: ProjectModel;
+  affairsCount: number;
+  projectManagerName: string;
+  company?: 'amexia' | 'diagobat' | 'imperium' | 'probim' | 'projex';
 };
 
-const ProjectCard = ({ project }: Props) => {
+const ProjectCard = ({ project, projectManagerName, affairsCount, company }: Props) => {
+  const getImageSrc = () => {
+    switch (company) {
+      case 'amexia':
+        return logoAmexia.src;
+      case 'diagobat':
+        return logoDiagobat.src;
+      case 'imperium':
+        return logoImperium.src;
+      case 'probim':
+        return logoProbim.src;
+      case 'projex':
+        return logoProjex.src;
+      default:
+        return logoGroupeProjex.src;
+    }
+  };
+
   return (
     <ShadowCard>
       <div className={styles.projectCard}>
@@ -21,10 +46,10 @@ const ProjectCard = ({ project }: Props) => {
         <section className={styles.content}>
           <h4 className={styles.title}>{project.name}</h4>
           <span>{project.client_company_name}</span>
-          <span>Chef de projet</span>
-          <span>Nombre d'affaires</span>
+          <span>{projectManagerName}</span>
+          <span>{affairsCount} affaires</span>
           <div className={styles.logoContainer}>
-            <img className={styles.logo} src={logoProjex.src} alt="Logo" />
+            <img className={styles.logo} src={getImageSrc()} alt="Logo" />
           </div>
         </section>
       </div>
