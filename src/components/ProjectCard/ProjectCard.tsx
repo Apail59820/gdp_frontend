52d28da4572e58b8a1224dfbfbb4bb174e0f3a1a
Edgar Cresson <e.cresson@groupe-projex.fr>
2023-01-10 14:19:04 +0100

    Add models and ProjectCard component

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
 import styles from './ProjectCard.module.scss';
 import { ShadowCard } from '@projex/ui';
+import type { ProjectModel } from '../../../models/ProjectModel';
+import img from '../../../public/project-image.png';
+import logoProjex from '../../../public/logo-projex.svg';
 
 type Props = {
-  project: {
-    name: string;
-    client_company_name: string;
-    client_info: string;
-    address: string;
-    zip_code: string;
-    city: string;
-    country: string;
-  };
+  project: ProjectModel;
 };
 
 const ProjectCard = ({ project }: Props) => {
   return (
     <ShadowCard>
-      <div className={styles.projectCard}>ProjectCard</div>
+      <div className={styles.projectCard}>
+        <img
+          className={styles.image}
+          src={project.image || img.src}
+          alt={`Image illustrant le projet ${project.name}`}
+        />
+        <section className={styles.content}>
+          <h4 className={styles.title}>{project.name}</h4>
+          <span>{project.client_company_name}</span>
+          <span>Chef de projet</span>
+          <span>Nombre d'affaires</span>
+          <div className={styles.logoContainer}>
+            <img className={styles.logo} src={logoProjex.src} alt="Logo" />
+          </div>
+        </section>
+      </div>
     </ShadowCard>
   );
 };
