0b8c030b64f08d9279b630dc1aa7c1c50b64d68d
Edgar Cresson <e.cresson@groupe-projex.fr>
2023-01-11 17:12:38 +0100

    Removed ClientCard

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,6 @@ import styles from '../styles/Home.module.css';
 import { PageHeaderBanner } from '@projex/ui';
 
 export default function Home() {
-  const client = {clientName: 'eee', addressLine1: 'eeee', addressLine2: 'hieorze',postalCode: 59000, city: 'LILLE',country: 'France'  };
-  const contact = [{contactName: 'hohih', phoneNumber1: 232432, phoneNumber2: 567567576, mail: 'fiozefio@gmail.com'}, 
-  {contactName: 'DZEDEZ', phoneNumber1: 2342, phoneNumber2: 8888, mail: 'EEEE@gmail.com'},
-  {contactName: 'EZZA', phoneNumber1: 2332321, phoneNumber2: 99999, mail: 'Efezfezez@gmail.com'},
-];
-
   return (
     <>
       <PageHeaderBanner title="Bonjour, Edgar Cresson" />
