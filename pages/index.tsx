b4791bd43ff9870409da947cc5b904142a722114
Edgar Cresson <e.cresson@groupe-projex.fr>
2023-01-11 17:09:07 +0100

    Refined index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,8 @@
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import { PageHeaderBanner } from '@projex/ui';
-import ManagerCard from '../src/components/ManagerCard/ManagerCard';
-import CollaboratorCard from '../src/components/CollaboratorCard/CollaboratorCard';
 
 export default function Home() {
-  const USER = {
-    first_name: 'Edgar',
-    last_name: 'Cresson',
-    role: 'Développeur',
-    company: 'Diagobat',
-    email: 'e.c@g-p.fr',
-  };
-
   return (
     <>
       <PageHeaderBanner title="Bonjour, Edgar Cresson" />
@@ -26,12 +16,9 @@ export default function Home() {
           alignItems: 'center',
         }}
       >
-        <div style={{ width: '640px', height: '192px', marginBottom: '50px' }}>
+        {/* <div style={{ width: '640px', height: '192px', marginBottom: '50px' }}>
           <ManagerCard user={USER} />
-        </div>
-        <div style={{ width: '416px', height: '134px', marginBottom: '50px' }}>
-          <CollaboratorCard user={USER} />
-        </div>
+        </div> */}
       </div>
     </>
   );
