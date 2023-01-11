70c06fd6df9da1d446a1b215b27196751ef351ba
Edgar Cresson <e.cresson@groupe-projex.fr>
2023-01-11 17:07:40 +0100

    Created ManagerCard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,36 @@
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import { PageHeaderBanner } from '@projex/ui';
-import UserCard from '../src/components/UserCard/UserCard';
+import ManagerCard from '../src/components/ManagerCard/ManagerCard';
+import CollaboratorCard from '../src/components/CollaboratorCard/CollaboratorCard';
 
 export default function Home() {
+  const USER = {
+    first_name: 'Edgar',
+    last_name: 'Cresson',
+    role: 'Développeur',
+    company: 'Diagobat',
+    email: 'e.c@g-p.fr',
+  };
+
   return (
     <>
       <PageHeaderBanner title="Bonjour, Edgar Cresson" />
-      <div style={{ height: '100%', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <div style={{ width: '416px', height: '134px' }}>
-          <UserCard
-            user={{
-              first_name: 'Michel',
-              last_name: 'Martin',
-              role: 'Ingénieur',
-              company: 'Diagobat',
-              email: 'contact@client.fr',
-            }}
-          />
+      <div
+        style={{
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <div style={{ width: '640px', height: '192px', marginBottom: '50px' }}>
+          <ManagerCard user={USER} />
+        </div>
+        <div style={{ width: '416px', height: '134px', marginBottom: '50px' }}>
+          <CollaboratorCard user={USER} />
         </div>
       </div>
     </>
