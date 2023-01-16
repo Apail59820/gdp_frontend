d05f5af1a64aefcc573c8ae36e52c77a774431fb
GuillaumeGH <g.dupays@groupe-projex.fr>
2023-01-16 18:02:07 +0100

    reafactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,20 +6,27 @@ import { UserModel } from '../models/UserModels';
 import { ProjectModel } from '../models/ProjectModel';
 
 export default function Home() {
-  const client  : ProjectModel = {client_company_name: 'eee', address: 'eeee',zip_code: '59000', city: 'LILLE',country: 'France'  };
-  const contact : UserModel[] = [{first_name: 'DZEDEZ', last_name: 'frzefze', number: '232432', email: 'fiozefio@gmail.com'}, 
-  {first_name: 'DZEDEZ', last_name: 'frzefze', number: '2342', email: 'EEEE@gmail.com'},
-  {first_name: 'EZZA', last_name: ' regert', number: '2332321', email: 'grgerg@gmail.com'},
-  {first_name: 'EZAEAZ', last_name: 'jyuy', number: '576', email: 'gggg@gmail.com'},
-  {first_name: 'fezfze', last_name: 'frzefze',number: '789',  email: 'fiozefio@gmail.com'}, 
-  {first_name: 'gggg', last_name: 'fzefze',number: '2342',  email: 'EEEE@gmail.com'},
-  {first_name: 'htyhty', last_name: 'jyujuy',number: '98709', email: 'Efezfezez@gmail.com'},
-  {first_name: 'cccc', last_name: 'ezz',number: '476547',email: 'azzzz@gmail.com'},
-  {first_name: 'zzzz', last_name: 'nfgdnbdgf',number: '999',  email: 'fiozefio@gmail.com'}, 
-  {first_name: 'loliolo', last_name: 'ltkrekop',number: '2342', email: 'EEEE@gmail.com'},
-  {first_name: 'aaaa', last_name: 'frzefze',number: '2332321',  email: 'Efezfezez@gmail.com'},
-  {first_name: 'wwwww', last_name: 'moptore',number: '222',  email: 'Efezfezez@gmail.com'}
-];
+  const client: ProjectModel = {
+    client_company_name: 'eee',
+    address: 'eeee',
+    zip_code: '59000',
+    city: 'LILLE',
+    country: 'France',
+  };
+  const contact: UserModel[] = [
+    { first_name: 'DZEDEZ', last_name: 'frzefze', number: '232432', email: 'fiozefio@gmail.com' },
+    { first_name: 'DZEDEZ', last_name: 'frzefze', number: '2342', email: 'EEEE@gmail.com' },
+    { first_name: 'EZZA', last_name: ' regert', number: '2332321', email: 'grgerg@gmail.com' },
+    { first_name: 'EZAEAZ', last_name: 'jyuy', number: '576', email: 'gggg@gmail.com' },
+    { first_name: 'fezfze', last_name: 'frzefze', number: '789', email: 'fiozefio@gmail.com' },
+    { first_name: 'gggg', last_name: 'fzefze', number: '2342', email: 'EEEE@gmail.com' },
+    { first_name: 'htyhty', last_name: 'jyujuy', number: '98709', email: 'Efezfezez@gmail.com' },
+    { first_name: 'cccc', last_name: 'ezz', number: '476547', email: 'azzzz@gmail.com' },
+    { first_name: 'zzzz', last_name: 'nfgdnbdgf', number: '999', email: 'fiozefio@gmail.com' },
+    { first_name: 'loliolo', last_name: 'ltkrekop', number: '2342', email: 'EEEE@gmail.com' },
+    { first_name: 'aaaa', last_name: 'frzefze', number: '2332321', email: 'Efezfezez@gmail.com' },
+    { first_name: 'wwwww', last_name: 'moptore', number: '222', email: 'Efezfezez@gmail.com' },
+  ];
 
   return (
     <>
@@ -35,7 +42,7 @@ export default function Home() {
               email: 'contact@client.fr',
             }}
           /> */}
-          <ClientCard client={client} users={contact} iconsToShow={5}></ClientCard>
+          <ClientCard client={client} users={contact} maxIcon={5}></ClientCard>
         </div>
       </div>
     </>
--- src/components/ClientCard/ClientCard.tsx
+++ src/components/ClientCard/ClientCard.tsx
@@ -1,107 +1,120 @@
-import React, { ElementType, ImgHTMLAttributes, ReactNode, useRef } from 'react';
+import React from 'react';
 import styles from './ClientCard.module.scss';
-import { useEffect, useState } from 'react'
+import { useState } from 'react';
 import { ShadowCard } from '@projex/ui';
 import Img from './portraitA.png';
-import { Tooltip, Button } from 'antd';
+import { Tooltip } from 'antd';
 import type { UserModel } from '../../../models/UserModels';
 import type { ProjectModel } from '../../../models/ProjectModel';
-import kebabMenu from '../../../public/ellipsis-vertical.svg';
+import KebabMenuForCards from '../KebabMenuForCards/KebabMenuForCards';
 
 type Props = {
-    client: ProjectModel;
-    users: UserModel[];
-    iconsToShow: number;
-    // to define
-    onClick?: React.MouseEventHandler;
-    // redirect to profiles
-    redirectTo?: React.MouseEventHandler;
+  client: ProjectModel;
+  users: UserModel[];
+  maxIcon: number;
+  // to define
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  // redirect to profiles
+  hrefUser?: string;
 };
 
-const ClientCard = ({ client, users, iconsToShow, onClick, redirectTo }: Props) => {
-    const [current, setCurrent] = useState(0);
-    const [nextCurrent, setNextCurrent] = useState<number | undefined>(undefined);
+const ClientCard = ({ client, users, maxIcon, onClick, hrefUser }: Props) => {
+  const [current, setCurrent] = useState(0);
+  const [nextCurrent, setNextCurrent] = useState<number | undefined>(undefined);
 
-    const nextPerson= (key: number) => {
-        key === 0 ? [setCurrent(key), setNextCurrent(undefined)] : setNextCurrent(key);
-        setTimeout(() => {
-            setNextCurrent(undefined)
-            setCurrent(key);
-        }, 300);
-      };
+  const nextPerson = (key: number) => {
+    key === 0 ? [setCurrent(key), setNextCurrent(undefined)] : setNextCurrent(key);
+    setTimeout(() => {
+      setNextCurrent(undefined);
+      setCurrent(key);
+    }, 300);
+  };
 
-    
-    return (
-        <>
-        <div className={styles.containerClientCard}>
-            <ShadowCard>
-                <div className={styles.clientCard}>
-                    
-                    <div className={styles.boxLeft}>
-                        <div className={styles.title}>{client.client_company_name}</div>
-                        <div className={styles.adress}>{client.address}</div>
-                        <div className={styles.adress}>{client.zip_code}</div>
-                        <div className={styles.adress}>{client.city}</div>
-                        <div className={styles.adress}>{client.country}</div>
+  return (
+    <>
+      {/* <div className={styles.containerClientCard}> */}
+      <ShadowCard width="40rem" height="12rem">
+        <div className={styles.clientCard}>
+          <section className={styles.clientDetails}>
+            <h1 className={styles.title}>{client.client_company_name}</h1>
+            <span className={styles.adress}>{client.address}</span>
+            <span className={styles.adress}>{client.zip_code}</span>
+            <span className={styles.adress}>{client.city}</span>
+            <span className={styles.adress}>{client.country}</span>
+          </section>
+          <div>
+            <div className={styles.slider}>
+              <section className={`${styles.sliderContent} ${nextCurrent !== undefined ? styles.slideAnime : ''}`}>
+                <div className={styles.container}>
+                  <div className={styles.contactAvatar}>
+                    <img
+                      src={users[current]?.avatar ? users[current].avatar : Img.src}
+                      alt={users[current]?.avatar ? '' : 'image contact'}
+                      className={styles.contactAvatarImg}
+                    />
+                  </div>
+                  <div>
+                    <div className={styles.clientName}>
+                      {users[current]?.first_name?.toUpperCase()} {users[current]?.last_name?.toUpperCase()}
                     </div>
-                    <div >
-                    <div className={styles.slider}>
-                        <div className={`${styles.sliderContent} ${nextCurrent !== undefined ? styles.slideAnime : ''}`}>
-                      
-                            <div className={styles.container}>
-                                <div className={styles.boxImg}>
-                                    <img src={users[current]?.avatar ? users[current].avatar  : Img.src} alt={users[current]?.avatar ? '' : 'image contact'} className={styles.contactImg}/>
-                                </div>
-                                <div>
-                                    <div className={styles.clientName}>{users[current]?.first_name?.toUpperCase()} {users[current]?.last_name?.toUpperCase()}</div>
-                                    <div >{users[current]?.number}</div>
-                                    {/* second phone number to added in the table */}
-                                    {/* <div >{contacts[current]?.number}</div> */}
-                                    <div >{users[current]?.email}</div>
-                                </div>
-
-                            </div>
-                            {nextCurrent && (                            
-                            <div className={styles.container}>
-                                <div className={styles.boxImg}>
-                                    <img src={users[nextCurrent]?.avatar ? users[nextCurrent].avatar  : Img.src} alt={users[nextCurrent]?.avatar ? '' : 'image contact'} className={styles.contactImg}/>
-                                </div>
-                                <div>
-                                    <div className={styles.clientName}>{users[nextCurrent]?.first_name?.toUpperCase()} {users[current]?.last_name?.toUpperCase()}</div>
-                                    <div >{users[nextCurrent]?.number}</div>
-                                        {/* second phone number to added in the table */}
-                                    {/* <div >{contacts[nextCurrent]?.number}</div> */}
-                                    <div >{users[nextCurrent]?.email}</div>
-                                </div>
-                            </div>
-                            )}
-                        </div>
-                    </div>
-
-                       
-                        <div className={styles.boxBottomRight}>
-                            {users.map((e: UserModel, key: number) => {
-                                if (key < iconsToShow) {
-                                    return (
-                                        <Tooltip placement="bottomLeft" title={e.first_name + ' ' + e.last_name}>
-                                            <img onClick={() => nextPerson(key)} key={key} src={users[current]?.avatar ? users[current].avatar  : Img.src} alt={users[current]?.avatar ? '' : 'image contact'} className={styles.contactBtn}></img>
-                                        </Tooltip>
-                                    )
-                                }
-                            })}
-                            {users.length >  iconsToShow &&
-                                <button className={styles.contactBtn}><div className={styles.contactBtnTxt}>{'+'+ `${users.length - iconsToShow}`}</div></button>
-                            }
-                        </div>
-                    </div>
-                    <button className={styles.kebabButton} onClick={onClick}>
-                        <img src={kebabMenu.src} alt={`Kebab menu for ${client.name}`} />
-                    </button>
+                    <div>{users[current]?.number}</div>
+                    {/* second phone number to added in the table */}
+                    {/* <div >{contacts[current]?.number}</div> */}
+                    <span>{users[current]?.email}</span>
+                  </div>
                 </div>
-            </ShadowCard>
+                {nextCurrent && (
+                  <div className={styles.container}>
+                    <div className={styles.contactAvatar}>
+                      <img
+                        src={users[nextCurrent]?.avatar ? users[nextCurrent].avatar : Img.src}
+                        alt={users[nextCurrent]?.avatar ? '' : 'image contact'}
+                        className={styles.contactAvatarImg}
+                      />
+                    </div>
+                    <div>
+                      <div className={styles.clientName}>
+                        {users[nextCurrent]?.first_name?.toUpperCase()} {users[current]?.last_name?.toUpperCase()}
+                      </div>
+                      <div>{users[nextCurrent]?.number}</div>
+                      {/* second phone number to added in the table */}
+                      {/* <div >{contacts[nextCurrent]?.number}</div> */}
+                      <span>{users[nextCurrent]?.email}</span>
+                    </div>
+                  </div>
+                )}
+              </section>
             </div>
-         </>
-    )
+
+            <div className={styles.containerClientIcons}>
+              {users.map((e: UserModel, key: number) => {
+                if (key < maxIcon) {
+                  return (
+                    <Tooltip placement="bottomLeft" title={e.first_name + ' ' + e.last_name}>
+                      <img
+                        onClick={() => nextPerson(key)}
+                        key={key}
+                        src={users[current]?.avatar ? users[current].avatar : Img.src}
+                        alt={users[current]?.avatar ? '' : 'image contact'}
+                        className={styles.contactIconBtn}
+                      ></img>
+                    </Tooltip>
+                  );
+                }
+              })}
+              {users.length > maxIcon && (
+                <button className={styles.contactIconBtn} onClick={() => hrefUser}>
+                  <span className={styles.contactIconBtnTxt}>{'+' + `${users.length - maxIcon}`}</span>
+                </button>
+              )}
+            </div>
+          </div>
+          <KebabMenuForCards onClick={() => onClick}></KebabMenuForCards>
+        </div>
+      </ShadowCard>
+      {/* </div> */}
+    </>
+  );
 };
 
 export default ClientCard;
