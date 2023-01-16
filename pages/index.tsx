9d8534b13fc87ca15ea15feb4bf526e4880e1922
Edgar Cresson <e.cresson@groupe-projex.fr>
2023-01-16 10:40:56 +0100

    Moved PageHeaderBanner component into maia and refined it

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,11 @@
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import { PageHeaderBanner } from '@projex/ui';
-import InformationsSection from '../src/components/InformationsSection';
-import { CompanyEnum } from '../models/CompanyEnum';
+import PageHeaderBanner from '../src/components/PageHeaderBanner/PageHeaderBanner';
 
 export default function Home() {
   return (
     <>
-      <PageHeaderBanner title="Bonjour, Edgar Cresson" />
-      <div
-        style={{
-          height: '100%',
-          width: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <InformationsSection
-          informations={{
-            image: undefined,
-            clientName: '',
-            projetManager: '',
-            numberOfAffairs: undefined,
-            company: CompanyEnum.GROUPE_PROJEX,
-          }}
-        />
-      </div>
+      <PageHeaderBanner title="Bonjour, Olivier Le Baron" />
     </>
   );
 }
---
+++ src/components/PageHeaderBanner/MainMessage/MainMessage.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './MainMessage.module.scss';
+import { capitalize } from '../../../../utils/capitalize';
+import { ProjectModel } from '../../../../models/ProjectModel';
+import defaultImage from '../../../../public/default-affair-image.png';
+
+type Props = {
+  project: Partial<ProjectModel>;
+};
+
+const MainMessage = ({ project }: Props) => {
+  return (
+    <div className={styles.mainMessage}>
+      {project.image ? (
+        <div className={styles.imageContainer}>
+          {/* TODO Revoir image */}
+          <img src={defaultImage.src} alt="" />
+        </div>
+      ) : null}
+      <section className={styles.content}>
+        <h1 className={styles.title}>{project.name ? capitalize(project.name) : 'Projet'}</h1>
+        {project.client_info ? <h2 className={styles.subtitle}>{capitalize(project.client_info)}</h2> : null}
+      </section>
+    </div>
+  );
+};
+
+export default MainMessage;
