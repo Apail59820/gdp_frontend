91f91fe31a8cdb04e7198d31b727057b04a55afa
GuillaumeGH <g.dupays@groupe-projex.fr>
2023-01-18 17:35:23 +0100

    models

---
+++ Models/DirectusModel.ts
@@ -0,0 +1,15 @@
+export type QueryParameters = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  filter?: any;
+  aggregate?: any;
+  groupBy?: any;
+  sort?: string;
+  fields?: string;
+  search?: string;
+  limit?: string;
+  offset?: string;
+  page?: string;
+  deep?: string;
+  meta?: string;
+  export?: string;
+};
