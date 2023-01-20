808780be98f80175936a3ea745f11a6aa16d16bf
GuillaumeGH <g.dupays@groupe-projex.fr>
2023-01-20 17:18:33 +0100

    redux with fetch datas

--- store/features/affairsReducer.ts
+++ store/features/affairsReducer.ts
@@ -1,37 +1,34 @@
 import { createSelector, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { AffairModel } from '../../Models/AffairModel';
+import { AffairModel } from '../../models/AffairModel';
 // import { getAffairs } from '../../services/affairs.service';
 
 export interface AffairState {
-    dataAffairs: AffairModel[]
+  dataAffairs: AffairModel[];
 }
-
 
 const initialState: AffairState = {
-    dataAffairs: [
-        {
-            id: 1,
-            name: "Affaire spéciale",
-            city: "Wasquehal"
-        }
-    ]
-}
+  dataAffairs: [
+    {
+      id: 1,
+      name: 'Affaire spéciale',
+      city: 'Wasquehal',
+    },
+  ],
+};
 
 const affairSlice = createSlice({
-    name: 'affair',
-    initialState: initialState,
-    reducers: {
-        addAffair(state) {
-            // const testAffaireToAdd = {id: 3, name: "Affaire très particulière", city:"Lille"}
-            state.dataAffairs = [...state.dataAffairs];
-            console.log('addaffair', state.dataAffairs);
-            
-        }
-    }
-})
+  name: 'affair',
+  initialState: initialState,
+  reducers: {
+    addAffair(state) {
+      // const testAffaireToAdd = {id: 3, name: "Affaire très particulière", city:"Lille"}
+      state.dataAffairs = [...state.dataAffairs];
+      console.log('addaffair', state.dataAffairs);
+    },
+  },
+});
 
 //Action
-
 
 //Reducer
 export const { addAffair } = affairSlice.actions;
--- store/features/projectsReducer.ts
+++ store/features/projectsReducer.ts
@@ -1,35 +1,33 @@
 import { createSelector, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { ProjectModel } from '../../Models/ProjectModel';
+import { ProjectModel } from '../../models/ProjectModel';
 // import { getAffairs } from '../../services/affairs.service';
 
 export interface ProjectState {
-    projects: ProjectModel[]
+  projects: ProjectModel[];
 }
-
 
 const initialState: ProjectState = {
-    projects: []
-}
+  projects: [],
+};
 
 const projectSlice = createSlice({
-    name: 'projects',
-    initialState: initialState,
-    reducers: {
-        getAllProjects(state: ProjectState) {
-            const vals = {
-                id : 2,
-                project_manager: "Zinedine",
-                project_name: "Ajouter des joueurs"
-            }
-            state.projects = [...state.projects]
-            console.log('getallproject', state.projects)         
-        },
-        addProject(state: ProjectState, action: PayloadAction<ProjectModel>) {
-            state.projects =  [...state.projects, action.payload]
-        }
-    }
-})
-
+  name: 'projects',
+  initialState: initialState,
+  reducers: {
+    getAllProjects(state: ProjectState) {
+      const vals = {
+        id: 2,
+        project_manager: 'Zinedine',
+        project_name: 'Ajouter des joueurs',
+      };
+      state.projects = [...state.projects];
+      console.log('getallproject', state.projects);
+    },
+    addProject(state: ProjectState, action: PayloadAction<ProjectModel>) {
+      state.projects = [...state.projects, action.payload];
+    },
+  },
+});
 
 //Reducer
 export const { getAllProjects, addProject } = projectSlice.actions;
