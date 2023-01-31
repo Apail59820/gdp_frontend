c2efffb40400c3cd8b2485a9a2d5be2696770174
Edgar Cresson <e.cresson@groupe-projex.fr>
2023-01-31 17:08:31 +0100

    Create TeamCard component

--- src/components/CollaboratorInformations/CollaboratorInformations.tsx
+++ src/components/CollaboratorInformations/CollaboratorInformations.tsx
@@ -9,7 +9,7 @@ type Props = {
 };
 
 const CollaboratorInformations = ({ user }: Props) => {
-  const { first_name, last_name, role, company, email } = user;
+  const { first_name, last_name, role, company, number, email } = user;
 
   const getColorByCompany = () => {
     switch (company?.toLowerCase()) {
@@ -39,8 +39,7 @@ const CollaboratorInformations = ({ user }: Props) => {
         {company ? capitalize(company) : ''}
       </span>
       <div className={styles.informations}>
-        {/* TODO Add tel */}
-        <a href="tel:">/</a>
+        <a href={`tel:${number}`}>{number}</a>
         {email ? <a href={`mailto:${email}`}>{email}</a> : null}
       </div>
     </div>
