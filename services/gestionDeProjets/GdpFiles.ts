4e957116565115846bb74d93691bc3cdf0361b80
GuillaumeGH <g.dupays@groupe-projex.fr>
2023-02-23 09:49:57 +0100

    added gdpFiles service

--- services/gestionDeProjets/GdpFiles.ts
+++ services/gestionDeProjets/GdpFiles.ts
@@ -9,6 +9,43 @@ const { publicRuntimeConfig } = getConfig();
 const defaultFields = ['*'].join(',');
 
 /**
+ * Retrieve gdp  files
+ * @returns Promise containing the request status and the gdp file corresponding to the id
+ * @param props queryParams
+ */
+export async function getGdpFiles(
+  props: QueryParameters = {}
+): Promise<{ status: number; data?: Partial<GdpFilesModel>[] }> {
+  if (!props.fields) props.fields = defaultFields;
+  const token = await retrieveToken();
+  if (!token) return Promise.resolve({ status: 401 });
+
+  const myHeaders = new Headers({
+    Authorization: `Bearer ${token}`,
+  });
+
+  const myInit: RequestInit = {
+    method: 'GET',
+    headers: myHeaders,
+    mode: 'cors',
+    cache: 'default',
+  };
+
+  return fetch(
+    `${publicRuntimeConfig.GESTION_DE_PROJET_API_URL}/files?${concatenateQueryParameters(props)}`,
+    myInit
+  ).then((res) => {
+    if (res.status === 200) {
+      return res.json().then((responseData) => {
+        return { status: res.status, data: responseData.data };
+      });
+    } else {
+      return { status: res.status };
+    }
+  });
+}
+
+/**
  * Retrieve a gdp file by id
  * @param id id of the gdp file
  * @param fields list of fields to retrieve.
