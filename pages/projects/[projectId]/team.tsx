6b11b8ef231aaf0e99211dcf2bf5cb8a8f100450
Edgar Cresson <e.cresson@groupe-projex.fr>
2023-03-27 12:05:01 +0200

    removed useless mock data

--- pages/projects/[projectId]/team.tsx
+++ pages/projects/[projectId]/team.tsx
@@ -4,26 +4,6 @@ import { GdpProjectsModel } from '../../../models/GdPModels';
 import { getGdpProjectById } from '../../../services/gestionDeProjets/GdpProjects';
 import TeamPage from '../../../src/TeamPage/TeamPage';
 
-// TODO
-const PROJECT_BY_ID: any = {
-  id: '1',
-  name: 'Nom du projet',
-  client_company_name: 'Nom du client',
-  client_info: undefined,
-  address: undefined,
-  zip_code: undefined,
-  city: undefined,
-  country: undefined,
-  image: 'ok',
-  status: undefined,
-  project_type: undefined,
-  company_entity: 'CompanyEnum.DIAGOBAT',
-  affairs: undefined,
-};
-
-const CLIENT_TEAM: any[] = [];
-const PROJECT_TEAM: any[] = [];
-
 const Team = () => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState<boolean>(true);
