063a470344eba72284e59dced1ca07c370f5846e
Edgar Cresson <e.cresson@groupe-projex.fr>
2023-06-16 16:12:11 +0200

    commit

--- pages/projects/[projectId]/affairs/[affairId]/index.tsx
+++ pages/projects/[projectId]/affairs/[affairId]/index.tsx
@@ -88,7 +88,7 @@ const Affair = () => {
                   };
                 });
 
-                setUsers(formattedUsers || []);
+                setUsers(formattedUsers);
               }
             });
           }
