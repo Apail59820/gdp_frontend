5de5d08413005262b9aa75bef3b438d5c064b53a
faycaltoure <f.toure@groupe-projex.fr>
2023-09-08 18:04:38 +0200

    :zap: Fix avg

--- src/components/SatisfactionCard/SatisfactionCard.tsx
+++ src/components/SatisfactionCard/SatisfactionCard.tsx
@@ -42,7 +42,7 @@ const SatisfactionCard = ({satisfactions, reverse}: SatisfactionCardProps) => {
     <ShadowCard>
       <div className={`${styles.container} ${reverse && styles.reverse}`}>
         <ProgressBarRounded
-          percentage={isNaN((score / total) * 100) ? 0 : (parseFloat((score / total).toFixed(2)))}
+          percentage={isNaN((score / total) * 100) ? 0 : (parseFloat((score / total * 100).toFixed(2)))}
           // text={`${score} / ${total}`}
           // color={'green'}
         />
