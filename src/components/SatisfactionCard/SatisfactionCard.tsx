e5bda8e6a78ecb869acd18aebe87d23be8624d04
faycaltoure <f.toure@groupe-projex.fr>
2023-09-17 15:32:54 +0200

    :zap: Fix All Week DSMS
    ...

--- src/components/SatisfactionCard/SatisfactionCard.tsx
+++ src/components/SatisfactionCard/SatisfactionCard.tsx
@@ -9,20 +9,20 @@ interface SatisfactionCardProps {
   reverse?: boolean;
 }
 
-const SatisfactionCard = ({satisfactions, reverse}: SatisfactionCardProps) => {
+const SatisfactionCard = ({ satisfactions, reverse }: SatisfactionCardProps) => {
   const total = 4;
 
   const score = isNaN(
     satisfactions.reduce(
       (acc, satisfaction) => acc + (satisfaction.score_hard_skills ? satisfaction.score_hard_skills : 0),
-      0,
-    ) / satisfactions.length,
+      0
+    ) / satisfactions.length
   )
     ? 0
     : satisfactions.reduce(
-    (acc, satisfaction) => acc + (satisfaction.score_hard_skills ? satisfaction.score_hard_skills : 0),
-    0,
-  ) / satisfactions.length;
+        (acc, satisfaction) => acc + (satisfaction.score_hard_skills ? satisfaction.score_hard_skills : 0),
+        0
+      ) / satisfactions.length;
 
   const commentsNumber = satisfactions.filter((satisfaction) => satisfaction.comment).length;
 
@@ -33,24 +33,24 @@ const SatisfactionCard = ({satisfactions, reverse}: SatisfactionCardProps) => {
   const lastComment =
     satisfactions.length > 0
       ? satisfactions.sort((a, b) => {
-        if (!a.date_created || !b.date_created) return 0;
-        return new Date(b.date_created).getTime() - new Date(a.date_created).getTime();
-      })[0]
+          if (!a.date_created || !b.date_created) return 0;
+          return new Date(b.date_created).getTime() - new Date(a.date_created).getTime();
+        })[0]
       : satisfactions[0];
 
   return (
     <ShadowCard>
       <div className={`${styles.container} ${reverse && styles.reverse}`}>
         <ProgressBarRounded
-          percentage={isNaN((score / total) * 100) ? 0 : (parseFloat((score / total * 100).toFixed(2)))}
-          // text={`${score} / ${total}`}
+          percentage={isNaN((score / total) * 100) ? 0 : (score / total) * 100}
+          text={`${(score / total).toFixed(2) as any *100}%`}
           // color={'green'}
         />
         <div className={styles.infos}>
           <span>
             Note moyenne :{' '}
             <b>
-              {score.toFixed((2))} / {total}
+              {score.toFixed(2)} / {total}
             </b>
           </span>
           <span>
