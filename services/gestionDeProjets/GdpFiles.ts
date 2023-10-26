61f2cbc12188c970bb5fcb3b5103b8bc5471536c
Amaury Paillart <amaury.paillart@groupe-projex.fr>
2023-10-26 17:36:52 +0200

    now uses signed url only if filesize > 32Mo

--- services/gestionDeProjets/GdpFiles.ts
+++ services/gestionDeProjets/GdpFiles.ts
@@ -260,7 +260,6 @@ export async function uploadGdpFileWithProgress(
 
   let exceptSignedUrl = ((file.properties.filesize / (1024 * 1024)) > 32);
 
-  exceptSignedUrl = true;
   let fetchUrl = publicRuntimeConfig.GESTION_DE_PROJET_API_URL + ((exceptSignedUrl) ? '/signed-url/write' : '/files');
 
   return await axiosInstance.post(fetchUrl, formData).then(async(res) => {
