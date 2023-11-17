360b1110a8917f57b51877f3a5453487f67b02d2
Amaury Paillart <amaury.paillart@groupe-projex.fr>
2023-11-17 07:57:29 +0100

    added search option to select affair

--- src/components/SelectAffair/SelectAffair.tsx
+++ src/components/SelectAffair/SelectAffair.tsx
@@ -5,6 +5,9 @@ import {GdpProjectsModel} from "../../../models/GestionDeProjets/GdpProjectsMode
 import {GdpAffairModel} from "../../../models/GestionDeProjets/GdpAffairModel";
 import {getGdpAffairs} from "../../../services/gestionDeProjets/GdpAffairs";
 import {isRequestSuccessful} from "../../../utils/isRequestSuccessful";
+import styles from "../ManageAffairManagerForm/ManageAffairManagerForm.module.scss";
+import {useForm} from "antd/lib/form/Form";
+import {selectAffairs} from "../../../store/reducers/affairsReducer";
 
 interface SelectAffairProps {
     isOpen: boolean;
@@ -15,19 +18,24 @@ interface SelectAffairProps {
 const SelectAffair = ({isOpen, handleClose, onSelect, project} : SelectAffairProps) => {
 
     const [affairs, setAffairs] = useState<Partial<GdpAffairModel>[]>([]);
-    const [selectedAffair, setSelectedAffair] = useState<Partial<GdpAffairModel>>()
+    const [selectedAffair, setSelectedAffair] = useState<Partial<GdpAffairModel>>();
+    const [affairsSearchResult, setAffairSearchResult] = useState<Partial<GdpAffairModel>[]>([]);
+
+
+    const [form] = useForm();
 
     useEffect(() => {
         if(project.id){
             getGdpAffairs({filter: {projects_id: {_in : project.id}}}).then((res) => {
                 if(isRequestSuccessful(res.status) && res?.data.length){
                     setAffairs(res.data);
+                    setAffairSearchResult(res.data);
                 }
             })
         }
     }, []);
 
-    const onFinish = async () => {
+    const onFinish = async (values : any) => {
         if(!selectedAffair) return message.error("Veuillez choisir une affaire.");
 
         onSelect(selectedAffair);
@@ -42,31 +50,54 @@ const SelectAffair = ({isOpen, handleClose, onSelect, project} : SelectAffairPro
             title={'Selectionner une affaire pour ce client'}
             footer={
             <div style={{display: "flex"}}>
-            <Button small onClick={onFinish}>
+            <Button small htmlType={"submit"}>
                 Enregistrer
             </Button>
             <Button small style={"text"} onClick={handleClose}>Annuler</Button>
             </div>
         }
         >
-            {affairs && (
-                <Form.Item>
-                    <Select
-                        placeholder={"Choisir une affaire..."}
-                        onSelect={(value) => {
-                            getGdpAffairs({filter: {id: value}}).then((res) => {
-                                if(isRequestSuccessful(res.status) && res?.data.length){
-                                    setSelectedAffair(res.data[0]);
-                                }
-                            })
-                        }}
-                        options={affairs.map((affair) => ({
-                            label: affair.name,
-                            value: affair.id,
-                        }))}
-                    />
-                </Form.Item>
 
+            {affairs && (
+                <Form form={form} name={'selectAffairForm'} autoComplete={'off'} layout={'vertical'} onFinish={onFinish}>
+                    <Form.Item name={'selectedAffair'}>
+                        <Select
+                            placeholder={"Choisir une affaire..."}
+                            showSearch
+                            filterOption={false}
+                            options={affairsSearchResult
+                                .map((affair) => ({
+                                    label: affair.name,
+                                    value: affair.id,
+                                }))}
+                            onSearch={async (value) => {
+                                if(value.length >= 1){
+                                    const fetchAffairs = await getGdpAffairs(
+                                        {filter: {
+                                                _and: [{
+                                                    name: {_starts_with: value},
+                                                    projects_id: {_in : project.id}
+                                                }]
+                                            }}
+                                    );
+
+                                    if(isRequestSuccessful(fetchAffairs.status) && fetchAffairs?.data.length){
+                                        setAffairSearchResult(fetchAffairs.data);
+                                    }
+                                }
+                            }}
+                            onChange={(value) => {
+                                if (value) {
+                                    const tmp = form.getFieldValue('selectedAffair');
+                                    let selectedAffair = affairsSearchResult.filter((affair) => affair.id == tmp);
+                                    if(selectedAffair.length == 1) {
+                                        setSelectedAffair(selectedAffair[0]);
+                                    }
+                                }
+                            }}
+                        />
+                    </Form.Item>
+                </Form>
             )}
         </Modal>
     );
