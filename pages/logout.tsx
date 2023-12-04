a1ec84565989c725378c70b61a9b2d625e877011
Atman ALI <a.ali@groupe-projex.fr>
2023-12-04 16:56:17 +0100

    logout-page

---
+++ pages/logout.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import { logout } from '../services/auth';
+import getConfig from "next/config";
+import styles from './logout.module.css'
+
+const { publicRuntimeConfig } = getConfig();
+const Logout = () => {
+    const router = useRouter();
+
+    if (typeof window !== 'undefined') {
+        logout()
+            .then(() => {
+                router.push(`${publicRuntimeConfig.USER_SERVICE_URL}/login?r=${publicRuntimeConfig.GESTION_DE_PROJET_URL}/`);
+            })
+    }
+
+    return <div className={styles.waiting}>
+        <img src={'/logo-groupe-projex.svg'}/>
+        Vous serez déconnecté d'ici un instant
+    </div>;
+};
+
+Logout.getInitialProps = () => {
+    return {};
+};
+
+export default Logout;
