e252e7585b15bfadd5e659a5004d26641b86f2d1
Atman ALI <a.ali@groupe-projex.fr>
2024-03-20 11:20:18 +0100

    init functions

---
+++ utils/CERBEutils.ts
@@ -0,0 +1,68 @@
+// DATA ENTRY
+// generalites
+export const generalitiesTotalSurfacePlot = (): number => {
+    return 0
+}
+export const totalSurfaceFloor = (): number => {
+    return 0
+}
+export const countProjects = (): number => {
+    return 0
+}
+export const countCertifiedProjects = (): number => {
+    return 0
+}
+
+// biodiversite
+export const biodiversityTotalSurfacePlot = (): number => {
+    return 0
+}
+export const averageInitialCBS = (): number => {
+    return 0
+}
+export const averageProjectCBS = (): number => {
+    return 0
+}
+export const averageInitialCRTS = (): number => {
+    return 0
+}
+export const averageProjectCRTS = (): number => {
+    return 0
+}
+// energie
+export const totalRefCep = (): number => {
+    return 0
+}
+export const totalProjectCep = (): number => {
+    return 0
+}
+export const totalEnergyEconomy = (): number => {
+    return 0
+}
+export const totalRenewableEnergyPerSurfaceUnity = (): number => {
+    return 0
+}
+export const totalRenewableEnergy = (): number => {
+    return totalRenewableEnergyPerSurfaceUnity()*totalSurfaceFloor()
+}
+// carbonne
+export const totalReferenceCarbonFootprint = (): number => {
+    return 0
+}
+export const totalProjectCarbonFootprint = (): number => {
+    return 0
+}
+export const totalBioSourcedMaterial = (): number => {
+    return 0
+}
+// ressources
+export const totalRainwaterTankCapacity = (): number => {
+    return 0
+}
+export const averageInitialPlotPermeability = (): number => {
+    return 0
+}
+export const averageProjectPlotPermeability = (): number => {
+    return 0
+}
+// technique
