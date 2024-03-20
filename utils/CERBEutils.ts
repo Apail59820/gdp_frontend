07b3819ffbb8835f229981c41ef37507c7f1f9dd
Atman ALI <a.ali@groupe-projex.fr>
2024-03-20 14:43:21 +0100

    more abstract utils

--- utils/CERBEutils.ts
+++ utils/CERBEutils.ts
@@ -1,68 +1,12 @@
-// DATA ENTRY
-// generalites
-export const generalitiesTotalSurfacePlot = (): number => {
-    return 0
+export const sum = (arrayOfNumbers: number[]): number => {
+    return arrayOfNumbers.reduce(
+        (partialSum, a) => partialSum+a,
+        0
+    )
 }
-export const totalSurfaceFloor = (): number => {
-    return 0
+export const average = (arrayOfNumbers: number[]): number => {
+    return arrayOfNumbers.length ? sum(arrayOfNumbers)/arrayOfNumbers.length : 0
 }
-export const countProjects = (): number => {
-    return 0
+export const ratio = (measureOfPart, measureOfAll): number => {
+    return measureOfAll==0 ? NaN : (1 - measureOfPart/measureOfAll) * 100;
 }
-export const countCertifiedProjects = (): number => {
-    return 0
-}
-
-// biodiversite
-export const biodiversityTotalSurfacePlot = (): number => {
-    return 0
-}
-export const averageInitialCBS = (): number => {
-    return 0
-}
-export const averageProjectCBS = (): number => {
-    return 0
-}
-export const averageInitialCRTS = (): number => {
-    return 0
-}
-export const averageProjectCRTS = (): number => {
-    return 0
-}
-// energie
-export const totalRefCep = (): number => {
-    return 0
-}
-export const totalProjectCep = (): number => {
-    return 0
-}
-export const totalEnergyEconomy = (): number => {
-    return 0
-}
-export const totalRenewableEnergyPerSurfaceUnity = (): number => {
-    return 0
-}
-export const totalRenewableEnergy = (): number => {
-    return totalRenewableEnergyPerSurfaceUnity()*totalSurfaceFloor()
-}
-// carbonne
-export const totalReferenceCarbonFootprint = (): number => {
-    return 0
-}
-export const totalProjectCarbonFootprint = (): number => {
-    return 0
-}
-export const totalBioSourcedMaterial = (): number => {
-    return 0
-}
-// ressources
-export const totalRainwaterTankCapacity = (): number => {
-    return 0
-}
-export const averageInitialPlotPermeability = (): number => {
-    return 0
-}
-export const averageProjectPlotPermeability = (): number => {
-    return 0
-}
-// technique
