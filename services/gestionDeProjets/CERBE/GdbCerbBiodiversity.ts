937b0136d4f0326e7644d43cd2724ea58102fce7
Amaury Paillart <amaury.paillart@groupe-projex.fr>
2024-03-20 16:09:01 +0100

    Init services

---
+++ services/gestionDeProjets/CERBE/GdbCerbBiodiversity.ts
@@ -0,0 +1,145 @@
+import { QueryParameters } from "../../../models/DirectusModel";
+import { retrieveToken } from "../../auth";
+import concatenateQueryParameters from "../../../utils/queryParamsFormatter";
+import getConfig from "next/config";
+import { GdpCerbBiodiversityModel } from "../../../models/GestionDeProjets/CERBE/GdpCerbBiodiversityModel";
+import { isRequestSuccessful } from "../../../utils/isRequestSuccessful";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { message } from "antd";
+
+const { publicRuntimeConfig } = getConfig();
+
+const defaultFields = ["*", "affairs_id.*"].join(",");
+
+export async function getGdpCerbBiodiversity(
+  props: QueryParameters = {},
+): Promise<{ status: number; data?: Partial<GdpCerbBiodiversityModel>[] }> {
+  if (!props.fields) props.fields = defaultFields;
+  const token = await retrieveToken();
+
+  if (!token) return Promise.resolve({ status: 401 });
+  const myHeaders = new Headers({
+    Authorization: `Bearer ${token}`,
+  });
+
+  const myInit: RequestInit = {
+    method: "GET",
+    headers: myHeaders,
+    mode: "cors",
+    cache: "default",
+  };
+
+  return fetch(
+    `${publicRuntimeConfig.GESTION_DE_PROJET_API_URL}/items/cerb_biodiversity?${concatenateQueryParameters(props)}`,
+    myInit,
+  )
+    .then((res) => {
+      if (res.status == 200)
+        return res.json().then((data) => {
+          return { status: res.status, data: data.data };
+        });
+      else return { status: res.status };
+    })
+    .catch(() => {
+      return { status: 500 };
+    });
+}
+
+type createFieldsToOmit =
+  | "id"
+  | "user_created"
+  | "date_created"
+  | "user_updated"
+  | "date_updated";
+export async function createGdpCerbBiodiversity(
+  gdpAffair: Omit<GdpCerbBiodiversityModel, createFieldsToOmit>,
+): Promise<{ status: number; data?: Partial<GdpCerbBiodiversityModel> }> {
+  const token = await retrieveToken();
+  if (!token) return Promise.resolve({ status: 401 });
+  const myHeaders = new Headers({
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
+  });
+
+  const myInit: RequestInit = {
+    method: "POST",
+    headers: myHeaders,
+    mode: "cors",
+    cache: "default",
+    body: JSON.stringify(gdpAffair),
+  };
+
+  return fetch(
+    `${publicRuntimeConfig.GESTION_DE_PROJET_API_URL}/items/cerb_biodiversity`,
+    myInit,
+  )
+    .then((response) => {
+      if (response.status === 200 || response.status === 204) {
+        return response
+          .json()
+          .then((responseData) => {
+            return { status: response.status, data: responseData.data };
+          })
+          .catch((error) => {
+            // eslint-disable-next-line no-console
+            console.error(error);
+            return { status: response.status };
+          });
+      } else {
+        return { status: response.status };
+      }
+    })
+    .catch((error) => {
+      // eslint-disable-next-line no-console
+      console.error(error);
+      return { status: 500 };
+    });
+}
+
+const fetchGdpCerbBiodiversity = async (params: QueryParameters) => {
+  const res = await getGdpCerbBiodiversity(params);
+  if (!isRequestSuccessful(res.status)) {
+    throw new Error("Network response was not ok");
+  }
+  return res.data;
+};
+
+export const useGdpCerbBiodiversity = (params: QueryParameters) => {
+  // @ts-ignore
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["cerb_biodiversity"],
+    queryFn: () => fetchGdpCerbBiodiversity(params),
+    staleTime: Infinity,
+  });
+
+  return {
+    isLoading: isLoading,
+    error: error,
+    cerb_biodiversity: data as Partial<GdpCerbBiodiversityModel>[],
+  };
+};
+
+export function useCreateGdpCerbBiodiversity() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (
+      newBiodiversity: Omit<GdpCerbBiodiversityModel, createFieldsToOmit>,
+    ) => {
+      return createGdpCerbBiodiversity(newBiodiversity);
+    },
+    onSuccess: async (data: any) => {
+      if (isRequestSuccessful(data.status)) {
+        await queryClient.invalidateQueries({
+          queryKey: ["cerb_biodiversity"],
+        });
+        message.success('Element "Biodiversité" ajouté avec succès.');
+      } else {
+        message.error("Une erreur est survenue.");
+      }
+    },
+    onError: (error: Error) => {
+      message.error("Une erreur est survenue.");
+    },
+  });
+}
