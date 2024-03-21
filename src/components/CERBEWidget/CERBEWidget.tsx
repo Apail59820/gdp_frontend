c9f5be11fd115b08a3e819620afa1f0c923ddce7
Atman ALI <a.ali@groupe-projex.fr>
2024-03-21 10:08:01 +0100

    fix: wrong mockData

--- src/components/CERBEWidget/CERBEWidget.tsx
+++ src/components/CERBEWidget/CERBEWidget.tsx
@@ -12,23 +12,11 @@ type Props = {
 }
 export default function CERBEWidget( { handleNewCERBERClick }: Props ) {
 
-    const mockProjects: {id: number; name: string}[] = [
-        { id: 1, name: 'mock Project 1' }
-    ];
     const onShowMyCERBEClick = ()=> {}
     return (
         <Section title="CERBE" button={{label: 'Voir tous les CERBE', onClick:onShowMyCERBEClick}}>
             <Grid>
-                {
-                    mockProjects.map((project) => (
-                        <Link key={project.id} href={`/projects/${project.id}`}>
-                            <ProjectCard project={project} projectManagerName={'Chef de projet'} />
-                        </Link>
-                    ))
-                }
-                <div className={styles.manageItemCardContainer}>
                     <ManageItemCard label="Nouveau CERBE" onClick={handleNewCERBERClick} />
-                </div>
             </Grid>
         </Section>
     );
