b5c440643dff0ed417bbbc3cae9a8a18fbe41585
Atman ALI <a.ali@groupe-projex.fr>
2024-03-22 16:57:44 +0100

    timer is functionnal

--- src/components/CerbeBannerTimer/CerbeBannerTimer.tsx
+++ src/components/CerbeBannerTimer/CerbeBannerTimer.tsx
@@ -1,26 +1,27 @@
-import styles from './CerbeBannerTimer.module.scss';
-import DiagonalPict from '../../../public/logo-diagobat.svg';
-import HeaderIcon from '../../../public/icon-cerbe-head.svg';
-import React, { useState } from 'react';
-import Timer from './Timer';
+import styles from "./CerbeBannerTimer.module.scss";
+import DiagonalPict from "../../../public/logo-diagobat.svg";
+import HeaderIcon from "../../../public/icon-cerbe-head.svg";
+import React, { useState } from "react";
+import Timer from "./Timer";
+import { Button } from "projex-ui";
+import Image from 'next/image';
 
 export default function CerbeBannerTimer() {
-
-    return (
-      <>
-        <div className={styles.container}>
-          <div>
-            <img src={DiagonalPict.src} alt="Diagobat Logo" />
-            <h4>
-                CERBE 2024 dans {' '}
-                <Timer secondsToWait={30} />
-            </h4>
-          </div>
-          <img src={HeaderIcon.src} alt="Diagobat vectorial" />
-          <div>
-            <button className={styles.goToButton}>go to somewhere</button>
-          </div>
+  const timestamp = new Date("1 apr 2024 9:0:0");
+  return (
+    <>
+      <div className={styles.container}>
+        <div>
+          <Image src={DiagonalPict.src} alt="Diagobat Logo" width={258} height={48} />
+          <h5>
+            CERBE 2024 dans <Timer expiryTimestamp={timestamp} />
+          </h5>
         </div>
-      </>
-    );
+        <img src={HeaderIcon.src} alt="Diagobat vectorial" />
+        <div>
+          <Button style={"text_gray"}>Go to somewhere</Button>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/components/CerbeBannerTimer/Timer.tsx
+++ src/components/CerbeBannerTimer/Timer.tsx
@@ -1,17 +1,19 @@
-import { useState } from 'react';
+import React from "react";
+import { useTimer } from "react-timer-hook";
 
-type Props = {
-    daysToWait?: number;
-    hoursToWait?: number;
-    minutesToWait?: number;
-    secondsToWait?: number;
-}
-
-export default function Timer({daysToWait, hoursToWait, minutesToWait, secondsToWait}: Props) {
-
-    return (
-        <>
-            {secondsToWait} seconds
-        </>
-    );
+export default function Timer({ expiryTimestamp }) {
+  const { seconds, minutes, hours, days, isRunning } = useTimer({
+    expiryTimestamp,
+    onExpire: () => console.warn("onExpire called"),
+  });
+  return (
+      <>
+        {days ? `${days} jours `: ''}
+        {hours ? `${hours} heures `: ''}
+        {minutes ? `${minutes} minutes `: ''}
+        {seconds ? `${seconds} secondes `: ''}
+        <br />
+        {isRunning? 'Veuillez remplir vos données CERBE avant le 1er avril' : ''}
+      </>
+  );
 }
